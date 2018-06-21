18a156e92acf136f0537a0865ec20e92d558f5b9
jiayoupengdan <3500325724@qq.com>
2018-06-21 14:07:12 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/peng/coolweather/util/Utility.java
+++ app/src/main/java/com/peng/coolweather/util/Utility.java
@@ -3,9 +3,11 @@ package com.peng.coolweather.util;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.peng.coolweather.db.City;
 import com.peng.coolweather.db.County;
 import com.peng.coolweather.db.Province;
+import com.peng.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,5 +79,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /*将返回的JSON数据解析成Weather实体类*/
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
